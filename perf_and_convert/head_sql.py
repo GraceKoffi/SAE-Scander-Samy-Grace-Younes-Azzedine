e4ada058c6771e5b42955d513d11af61052060fd
Samy <129394833+Samy93000@users.noreply.github.com>
2023-10-17 21:10:50 +0200

    Update head_sql.py

--- perf_and_convert/head_sql.py
+++ perf_and_convert/head_sql.py
@@ -1,6 +1,7 @@
 import re
 
-def sql_type(value):
+
+def sql_type_primary(value):
     if re.match(r'^[+-]?[0-9]+$', value):
         return "INTEGER"
     elif re.match(r'^[+-]?([0-9]*[.])?[0-9]+$', value):
@@ -15,13 +16,55 @@ def sql_type(value):
         return f"VARCHAR({len(value)})"
 
 
+
+
+
+def sql_type(value):
+    if ',' in value :
+
+        value_first_word = value.split(',')[0]
+        if re.match(r'^[+-]?[0-9]+$', value_first_word):
+            return "INTEGER"
+        elif re.match(r'^[+-]?([0-9]*[.])?[0-9]+$', value_first_word):
+            return "FLOAT"
+        elif re.match(r'^[0-1]$', value_first_word):
+            return "BOOLEAN"
+        elif re.match(r'^\d{4}-\d{2}-\d{2}$', value_first_word):
+            return "DATE"
+        elif re.match(r'^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$', value_first_word):
+            return "DATETIME"
+        else:
+            return f"VARCHAR(255)"
+    
+    
+    else :
+        if re.match(r'^[+-]?[0-9]+$', value):
+            return "INTEGER"
+        elif re.match(r'^[+-]?([0-9]*[.])?[0-9]+$', value):
+            return "FLOAT"
+        elif re.match(r'^[0-1]$', value):
+            return "BOOLEAN"
+        elif re.match(r'^\d{4}-\d{2}-\d{2}$', value):
+            return "DATE"
+        elif re.match(r'^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$', value):
+            return "DATETIME"
+        else:
+            return f"VARCHAR(255)"
+
+
 def generate_sql_header(file, name_table):
-   try:
+
+    try:
         with open(file, "r", encoding="utf8") as tsv_file:
             column_names = tsv_file.readline().strip().split('\t')
             primary_key = column_names[0].split()[0]
             types_line = tsv_file.readline().strip().split('\t')
-            column_types = [sql_type(value) for value in types_line]
+            column_types = []
+            column_types.append(sql_type_primary(types_line[0]))
+            for i in range(1,len(types_line)):
+                column_types.append(sql_type(types_line[i]))
+            
+            print(column_types)
 
             # Créer la commande CREATE TABLE
             create_table_command = f"CREATE TABLE IF NOT EXISTS {name_table} (\n"
@@ -43,4 +86,10 @@ def generate_sql_header(file, name_table):
     except Exception as e:
         print(f"Une erreur s'est produite lors de la génération de la commande CREATE TABLE : {str(e)}")
         return 
-    
+
+
+file = "test.tsv"
+
+name_table = "test"
+
+print(generate_sql_header(file, name_table))
