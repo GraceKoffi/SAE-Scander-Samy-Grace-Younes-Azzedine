3a14cd51882de08e8531da4c90b65d93610dfc32
Samy <129394833+Samy93000@users.noreply.github.com>
2023-10-20 18:03:58 +0200

    Update perf.py

--- perf_and_convert/perf.py
+++ perf_and_convert/perf.py
@@ -1,42 +1,77 @@
 from time import time
 import subprocess
 import os
+import sys
 
 try:
  
     path_script = input("Veuillez saisir le chemin d'accès au script :\n ")
-    
+
+
     if not os.path.exists(path_script) :
         raise FileNotFoundError(f"Le fichier spécifié n'existe pas : {path_script}\n")
     
-    fichier_tsv = input("Veuillez saisir le chemin d'accès au fichier tsv à convertir :\n ")
+    if "recup_n.sh" in path_script :
+        nb = input("Veuillez saisir le nombre de ligne :\n")
+        fichier_tsv = input("Veuillez saisir le chemin d'accès au fichier tsv à convertir :\n ")
     
-    if not os.path.exists(fichier_tsv) :
-        raise FileNotFoundError(f"Le fichier spécifié n'existe pas : {fichier_tsv}\n")
+        if not os.path.exists(fichier_tsv) :
+            raise FileNotFoundError(f"Le fichier spécifié n'existe pas : {fichier_tsv}\n")
     
-    fichier_sql = input("Veuillez saisir le nom du fichier sql :\n ")
-    
+        fichier_sql = input("Veuillez saisir le nom du fichier sql :\n ")
+        tic = time()
 
-    # Mesure du temps de début
-    tic = time()
+        # Exécution du script shell avec les arguments
+        subprocess.run(["sh", path_script, fichier_tsv, fichier_sql, nb], stdout=subprocess.PIPE, text=True)
 
-    # Exécution du script shell avec les arguments
-    subprocess.run(["sh", path_script, fichier_tsv, fichier_sql], stdout=subprocess.PIPE, text=True)
-
-    # Mesure du temps de fin
-    tac = time()
-    seconde = tac - tic
-    if seconde < 60 :
+        # Mesure du temps de fin
+        tac = time()
+        seconde = tac - tic
+        if seconde < 60 :
         
-        print(f"Opération terminée avec succès en {round(seconde, 2)}s.\n")
+            print(f"Opération terminée avec succès en {round(seconde, 2)}s.\n")
+            sys.exit()
 
-    elif seconde >= 60 and seconde < 3600 :
-        minute = (tac - tic)/60
-        print(f"Opération terminée avec succès en {round(minute, 2)}m.\n")
-
+        elif seconde >= 60 and seconde < 3600 :
+            minute = (tac - tic)/60
+            print(f"Opération terminée avec succès en {round(minute, 2)}m.\n")
+            sys.exit()
+        else :
+            heure = (tac - tic)/3600
+            print(f"Opération terminée avec succès en {round(heure, 2)}h.\n")
+            sys.exit()
+    
+    
     else :
-        heure = (tac - tic)/3600
-        print(f"Opération terminée avec succès en {round(heure, 2)}h.\n")
+
+        fichier_tsv = input("Veuillez saisir le chemin d'accès au fichier tsv à convertir :\n ")
+    
+        if not os.path.exists(fichier_tsv) :
+            raise FileNotFoundError(f"Le fichier spécifié n'existe pas : {fichier_tsv}\n")
+    
+        fichier_sql = input("Veuillez saisir le nom du fichier sql :\n ")
+    
+
+        # Mesure du temps de début
+        tic = time()
+
+        # Exécution du script shell avec les arguments
+        subprocess.run(["sh", path_script, fichier_tsv, fichier_sql], stdout=subprocess.PIPE, text=True)
+
+        # Mesure du temps de fin
+        tac = time()
+        seconde = tac - tic
+        if seconde < 60 :
+        
+            print(f"Opération terminée avec succès en {round(seconde, 2)}s.\n")
+
+        elif seconde >= 60 and seconde < 3600 :
+            minute = (tac - tic)/60
+            print(f"Opération terminée avec succès en {round(minute, 2)}m.\n")
+
+        else :
+            heure = (tac - tic)/3600
+            print(f"Opération terminée avec succès en {round(heure, 2)}h.\n")
 
    
 
