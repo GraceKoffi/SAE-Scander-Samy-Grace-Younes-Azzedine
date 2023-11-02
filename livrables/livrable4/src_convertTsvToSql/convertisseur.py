7bbb6e1811fa8598aa2af55f7ffe41bf72ea47a5
Samy <129394833+Samy93000@users.noreply.github.com>
2023-11-03 00:04:20 +0100

    Add files via upload

---
+++ livrables/livrable4/src_convertTsvToSql/convertisseur.py
@@ -0,0 +1,163 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2023,
+# Tous droits réservés.
+#
+
+#
+#Consultez le fichier README pour les détails de l'utilisation.
+#
+# Le code ci-dessous est le contenu principal du fichier convertisseur.py.
+
+import subprocess, os, re
+from unzip import unzip
+from time import time
+
+
+
+try:
+    conversion_times = []
+    time_code_begin = time()
+
+    path_script = input("Veuillez saisir le chemin d'accès au script : ")
+
+
+    if not os.path.exists(path_script) :
+        raise FileNotFoundError(f"'{path_script}' ce fichier n'existe pas.")
+    
+    else:
+
+        if "recup_n.sh" in path_script :
+            nb_lignes = input("Veuillez saisir le nombre de ligne : ")
+            if not  re.match(r'^[0-9]+$', nb_lignes):
+                
+                raise TypeError(f"'{nb_lignes}' n'est pas un entier.")
+                
+            else :
+                gz = input("Veuillez saisir le chemin d'accès des ou du fichier gz : ")
+                fichiers_gz = gz.strip().split()
+                fichiers_tsv = []
+                for fichier_gz in fichiers_gz :
+                    if not os.path.exists(fichier_gz) :
+                        raise FileNotFoundError(f"'{fichier_gz}' n'existe pas.")
+                    elif not fichier_gz.endswith(".gz") :
+                        raise ValueError(f"'{fichier_gz}' n'est pas un fichier gz.")
+                    
+                    else :
+                        fichier_tsv = unzip(fichier_gz)
+                        if not fichier_tsv.endswith(".tsv"):
+                            raise ValueError(f"'{fichier_tsv}' n'est pas un fichier tsv.")
+                        
+                        elif os.path.getsize(fichier_tsv) == 0:
+
+                            raise ValueError(f"'{fichier_tsv}' est vide.")
+                        
+                        else :
+
+                            fichiers_tsv.append(fichier_tsv)
+
+
+                for fichier_tsv in fichiers_tsv :
+                    tic = time()
+                    fichier_sql = f"{fichier_tsv.split('.tsv')[0].replace(".","_")}.sql"
+
+                    # Exécution du script shell avec les arguments
+                    subprocess.run(["sh", path_script, fichier_tsv, fichier_sql, nb_lignes], stdout=subprocess.PIPE, text=True)
+                    tac = time()
+                    conversion_times.append(tac-tic)
+                   
+
+    
+        elif "recup_all.sh" in path_script :
+            gz = input("Veuillez saisir le chemin d'accès des ou du fichier gz : ")
+            fichiers_gz = gz.strip().split()
+            fichiers_tsv = []
+            for fichier_gz in fichiers_gz :
+                if not os.path.exists(fichier_gz) :
+
+                    raise FileNotFoundError(f"'{fichier_gz}' n'existe pas.")
+                elif not fichier_gz.endswith(".gz") :
+                    raise ValueError(f"'{fichier_gz}' n'est pas un fichier gz.")
+                
+                else :
+                    fichier_tsv = unzip(fichier_gz)
+                    if not fichier_tsv.endswith(".tsv"):
+                        raise ValueError(f"'{fichier_tsv}' n'est pas un fichier tsv.")
+                    
+                    elif os.path.getsize(fichier_tsv) == 0:
+
+                        raise ValueError(f"'{fichier_tsv}' est vide.")
+                    
+                    else :
+
+                        fichiers_tsv.append(fichier_tsv)
+
+
+            for fichier_tsv in fichiers_tsv :
+                tic = time()
+                fichier_sql = f"{fichier_tsv.split('.tsv')[0].replace(".","_")}.sql"
+
+                # Exécution du script shell avec les arguments
+                subprocess.run(["sh", path_script, fichier_tsv, fichier_sql], stdout=subprocess.PIPE, text=True)
+                tac = time()
+                conversion_times.append(tac-tic)
+                
+        else :
+            raise ValueError(f"'{path_script}' n'est pas un fichier de convertion.")
+    
+    
+
+
+   
+except FileNotFoundError as e :
+    print(f"KO: FileNotFoundError\nDescription: {e}")
+    
+except ValueError as e :
+    print(f"KO: ValueError\nDescription: {e}")
+    
+except TypeError as e :
+    print(f"KO: TypeError\nDescription: {e}")
+    
+except Exception as e:
+    print(f"KO: Exception\nDescription: {e}")
+    
+
+else :
+    # Mesure du temps de fin
+    time_convert = sum(conversion_times)
+    if time_convert < 60 :
+        s = ""
+        if len(fichiers_tsv) > 1 :
+            s="s"
+        print(f"OK convert in {round(time_convert, 2)}s  ({len(fichiers_tsv)} file{s}).")
+
+    elif time_convert >= 60 and time_convert < 3600 :
+        minute = time_convert/60
+        s = ""
+        if len(fichiers_tsv) > 1 :
+            s="s"
+        print(f"OK convert in {round(minute, 2)}m  ({len(fichiers_tsv)} file{s}).")
+
+    else :
+        heure = time_convert/3600
+        s = ""
+        if len(fichiers_tsv) > 1 :
+            s="s"
+        print(f"OK convert in {round(heure, 2)}h  ({len(fichiers_tsv)} file{s}).")
+
+    
+finally:
+    time_code_end = time()
+    time_code = time_code_end - time_code_begin
+    
+    if time_code < 60 :
+        print(f"Finish in {round(time_code, 2)}s.")
+
+    elif time_code >= 60 and time_code < 3600 :
+        minute = time_code/60
+        print(f"Finish in {round(minute, 2)}m.")
+
+    else :
+        heure = time_code/3600
+        print(f"Finish in {round(heure, 2)}h.")
+
---
+++ livrables/livrable4/src_convertTsvToSql/recup_n.sh
@@ -0,0 +1,130 @@
+#!/bin/bash
+
+# Vérifier le nombre d'arguments
+if [ "$#" -lt 3 ]; then
+  echo "Usage: $0 <fichier_tsv> <fichier_sql> <nombre_de_lignes>"
+  exit 1
+fi
+
+# Assigner les arguments à des variables
+fichier_tsv="$1"
+fichier_sql="$2"
+max_lignes="$3"  
+((max_lignes++)) # Ajout de 1 à nombre_lignes
+# Extraire le nom du fichier (enlever l'extension .sql)
+nom_table=$(basename "$fichier_sql" .sql)
+
+# Appeler le script Python pour obtenir la commande CREATE TABLE
+python -c "
+import re
+
+def sql_type_primary(value):
+    if re.match(r'^[+-]?[0-9]+$', value):
+        return 'INTEGER'
+    elif re.match(r'^[+-]?([0-9]*[.])?[0-9]+$', value):
+        return 'FLOAT'
+    elif re.match(r'^\d{4}-\d{2}-\d{2}$', value):
+        return 'DATE'
+    elif re.match(r'^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$', value):
+        return 'DATETIME'
+    else:
+        return 'VARCHAR(' + str(len(value)) + ') CHARACTER SET utf8 COLLATE utf8_general_ci'
+
+def sql_type(value):
+    if ',' in value:
+        value_first_word = value.split(',')[0]
+        
+        if re.match(r'^[+-]?[0-9]+$', value_first_word):
+            return 'INTEGER'
+        elif re.match(r'^[+-]?([0-9]*[.])?[0-9]+$', value_first_word):
+            return 'FLOAT'
+        
+        elif re.match(r'^\d{4}-\d{2}-\d{2}$', value_first_word):
+            return 'DATE'
+        elif re.match(r'^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$', value_first_word):
+            return 'DATETIME'
+        else:
+            return 'VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci'
+    else:
+        
+        if re.match(r'^[+-]?[0-9]+$', value):
+            return 'INTEGER'
+        elif re.match(r'^[+-]?([0-9]*[.])?[0-9]+$', value):
+            return 'FLOAT'
+        elif re.match(r'^\d{4}-\d{2}-\d{2}$', value):
+            return 'DATE'
+        elif re.match(r'^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$', value):
+            return 'DATETIME'
+        else:
+            return 'VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci'
+
+try:
+    with open('$fichier_tsv', 'r', encoding='utf8') as tsv_file:
+        
+        column_names = tsv_file.readline().strip().split('\t')
+        
+        types_line = tsv_file.readline().strip().split('\t')
+        column_types = []
+        
+       
+
+        create_table_command = 'CREATE TABLE IF NOT EXISTS ' + '$nom_table' + ' (\n'
+        
+        if '$nom_table' == 'title_akas' or '$nom_table' == 'title_principals':
+            create_table_command += '  id INT AUTO_INCREMENT PRIMARY KEY,\n'
+            for i in range(0, len(types_line)):
+                
+                column_types.append(sql_type(types_line[i]))
+        else :
+            primary_key = column_names[0].split()[0]
+            column_types.append(sql_type_primary(types_line[0]))
+            for i in range(1, len(types_line)):
+
+                column_types.append(sql_type(types_line[i]))
+        
+        for i in range(len(column_names)):
+            create_table_command += '  ' + column_names[i] + ' ' + column_types[i]
+            if i < len(column_names) - 1 :
+                create_table_command += ','
+            create_table_command += '\n'
+        create_table_command += ');'
+        
+
+        with open('$fichier_sql', 'w', encoding='utf8') as sql_file:
+            sql_file.write(create_table_command)
+            sql_file.write('\n')
+except Exception as e:
+    print('Une erreur s\'est produite lors de la génération de la commande CREATE TABLE : ' + str(e))
+"
+# Générer les INSERT INTO à partir du fichier TSV
+
+# Utiliser head pour extraire la première ligne du fichier TSV
+awk -F'\t' -v nom_table="$nom_table" -v max_lignes="$max_lignes" '
+  NR == 1 {
+    formatted_line = "(";
+    for (i = 1; i <= NF; i++) {
+      formatted_line = formatted_line $i;
+      if (i < NF) {
+        formatted_line = formatted_line ", ";
+      }
+    }
+    formatted_line = formatted_line ")";
+  }
+
+  NR > 1 && NR <= max_lignes {
+    printf "INSERT INTO %s %s VALUES (", nom_table, formatted_line;
+
+    for (i = 1; i <= NF; i++) {
+      if ($i == "\\N") {
+        printf "NULL";
+      } else {
+        printf "\047%s\047", $i;
+      }
+
+      if (i < NF) {
+        printf ",";
+      }
+    }
+    printf ");\n";
+  }
+' "$fichier_tsv" >> "$fichier_sql"
