22ea3d5b3a73111e097e781aaa15dac9a9332a90
Samy93000 <okisamycontactpro@gmail.com>
2023-12-02 20:30:52 +0100

    Update

---
+++ livrables/livrable4/src/bin/README.md
@@ -0,0 +1,90 @@
+# Convertisseur de fichiers TSV en SQL
+
+Ce projet propose un ensemble de scripts en shell (`bash`) pour faciliter la conversion de fichiers TSV en fichiers SQL à l'aide du programme Python `convertisseur.py`. Les scripts inclus sont `mouv.sh` et `supp.sh`.
+
+## Table des matières
+
+1. [Introduction](#introduction)
+2. [Configuration requise](#configuration-requise)
+3. [Scripts Inclus](#scripts-inclus)
+    - [Déplacement (`mouv.sh`)](#déplacement-mouvsh)
+    - [Suppression (`supp.sh`)](#suppression-suppsh)
+4. [Utilisation](#utilisation)
+5. [Exemples](#exemples)
+
+## Introduction
+
+Le projet comprend des scripts shell (`bash`) qui sont utiles en conjonction avec le programme Python `convertisseur.py`. Les deux scripts inclus sont `mouv.sh` pour le déplacement de fichiers et `supp.sh` pour la suppression de fichiers.
+
+## Configuration requise
+
+Avant d'utiliser ces scripts, assurez-vous d'avoir les dépendances suivantes installées :
+- Un interpréteur de commandes Bash compatible avec Windows, comme Git Bash ou Windows Subsystem for Linux (WSL).
+
+## Scripts Inclus
+
+### Déplacement (`mouv.sh`)
+
+```bash
+# Script de Déplacement (`mouv.sh`)
+
+Ce script en shell (`bash`) permet de déplacer des fichiers compressés (`.gz`) entre un répertoire source et le répertoire actuel (ou vice versa) en fonction de la direction spécifiée.
+```
+
+## Utilisation
+
+Pour utiliser le script, exécutez la commande suivante :
+
+```bash
+bash mouv.sh <mode> <répertoire_source>
+```
+- '`mode`' -> 'get' pour obtenir du répertoire source ou 'push' pour pousser vers le répertoire source.
+- '`répertoire_source`' -> Le chemin d'accès au répertoire source.
+
+
+## Exemples
+
+- Déplacer du Répertoire Source
+
+```bash
+bash mouv.sh get /chemin/vers/le/répertoire/source
+```
+
+Cette commande déplace tous les fichiers (`.gz`) du répertoire source vers le répertoire actuel.
+
+- Pousser vers le Répertoire Source
+
+```bash
+bash mouv.sh push /chemin/vers/le/répertoire/source
+```
+Cette commande déplace tous les fichiers .gz du répertoire actuel vers le répertoire source.
+
+
+
+### Suppression (`supp.sh`)
+
+```bash
+# Script de Suppression (`supp.sh`)
+
+Ce script en shell (`bash`) permet de supprimer les fichiers avec les extensions `.sql` et `.tsv` dans le répertoire actuel. Il enregistre également les fichiers supprimés et la date dans un fichier journal `log.log`.
+
+## Utilisation
+
+Pour utiliser le script, exécutez la commande suivante :
+
+`bash supp.sh`
+```
+Cette commande supprime tous les fichiers .sql et .tsv du répertoire actu
+
+
+## Utilisation
+
+Pour utiliser ces scripts, suivez les instructions spécifiques à chaque script mentionné ci-dessus. Assurez-vous d'avoir correctement configuré votre interpréteur de commandes Bash pour qu'il puisse exécuter ces scripts.
+
+## Exemples
+
+Des exemples détaillés sont fournis pour illustrer l'utilisation des scripts `mouv.sh` et `supp.sh`. Ces exemples vous guideront à travers les différentes commandes et options disponibles.
+
+**Note :** Assurez-vous d'avoir correctement configuré votre environnement et d'avoir les permissions nécessaires pour effectuer les opérations de déplacement et de suppression.
+
+
---
+++ livrables/livrable4/src/bin/recup_all.sh
@@ -0,0 +1,166 @@
+#!/bin/bash
+
+# Vérifier le nombre d'arguments
+if [ "$#" -ne 3 ]; then
+  echo "Usage: $0 <fichier_tsv> <fichier_sql>"
+  exit 1
+fi
+
+# Assigner les arguments à des variables
+fichier_tsv="$1"
+fichier_sql="$2"
+python_cmd="$3"
+
+# Extraire le nom du fichier (enlever l'extension .sql)
+nom_table=$(basename "$fichier_sql" .sql)
+
+
+
+
+
+# Appeler le script Python pour obtenir la commande CREATE TABLE
+$python_cmd -c "
+import re
+
+def sql_type_primary(value):
+    if re.match(r'^[+-]?[0-9]+$', value):
+        return 'INTEGER'
+    elif re.match(r'^[+-]?([0-9]*[.])?[0-9]+$', value):
+        return 'FLOAT'
+    elif re.match(r'^\d{4}-\d{2}-\d{2}$', value):
+        return 'DATE'
+    elif re.match(r'^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$', value):
+        return 'DATETIME'
+    else:
+        return 'VARCHAR(' + str(len(value)) + ')'
+
+def sql_type(value):
+    if ',' in value:
+        value_first_word = value.split(',')[0]
+        
+        if re.match(r'^[+-]?[0-9]+$', value_first_word):
+            return 'INTEGER[]'
+        elif re.match(r'^[+-]?([0-9]*[.])?[0-9]+$', value_first_word):
+            return 'DATE[]'
+        
+        elif re.match(r'^\d{4}-\d{2}-\d{2}$', value_first_word):
+            return 'FLOAT[]'
+        elif re.match(r'^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$', value_first_word):
+            return 'TIMESTAMP[]'
+        else:
+            return 'VARCHAR(255)[]'
+    else:
+        
+        if re.match(r'^[+-]?[0-9]+$', value):
+            return 'INTEGER'
+        elif re.match(r'^[+-]?([0-9]*[.])?[0-9]+$', value):
+            return 'FLOAT'
+        elif re.match(r'^\d{4}-\d{2}-\d{2}$', value):
+            return 'DATE'
+        elif re.match(r'^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$', value):
+            return 'TIMESTAMP'
+        else:
+            return 'VARCHAR(255)'
+
+try:
+    with open('$fichier_tsv', 'r', encoding='utf8') as tsv_file:
+        
+        column_names = tsv_file.readline().strip().split('\t')
+        
+        types_line = tsv_file.readline().strip().split('\t')
+        column_types = []
+        
+       
+
+        create_table_command ='DROP TABLE IF EXISTS ' + '$nom_table' + ';'
+        create_table_command +='\n'
+
+        
+        create_table_command += 'CREATE TABLE IF NOT EXISTS ' + '$nom_table' + ' (\n'
+        
+        if '$nom_table' == 'title_akas' or '$nom_table' == 'title_principals':
+            for i in range(0, len(types_line)):
+                
+                column_types.append(sql_type(types_line[i]))
+            
+            for i in range(len(column_names)):
+            
+                
+                if i < len(column_names) - 1 :
+                    create_table_command += '  ' + column_names[i] + ' ' + column_types[i]
+                    create_table_command += ','
+                else :
+                    create_table_command += '  ' + column_names[i] + ' ' + column_types[i]+','
+
+                create_table_command += '\n'
+
+            create_table_command += '  PRIMARY KEY('+column_names[0]+','+' '+column_names[1]+')\n'
+            create_table_command += ');'
+
+            
+        else :
+            primary_key = column_names[0].split()[0]
+            
+            column_types.append(sql_type_primary(types_line[0]))
+            for i in range(1, len(types_line)):
+
+                column_types.append(sql_type(types_line[i]))
+            
+            create_table_command += '  ' + column_names[0]+' '+ column_types[0] +' PRIMARY KEY,\n'
+            for i in range(1, len(column_names)):
+
+                create_table_command += '  ' + column_names[i] + ' ' + column_types[i]
+                if i < len(column_names) - 1 :
+                    create_table_command += ','
+                create_table_command += '\n'
+            create_table_command += ');'
+            
+        with open('$fichier_sql', 'w', encoding='utf8') as sql_file:
+            sql_file.write(create_table_command)
+            sql_file.write('\n')
+except Exception as e:
+    print('Une erreur s\'est produite lors de la génération de la commande CREATE TABLE : ' + str(e))
+"
+# Écrire les requêtes SQL à la fin du fichier SQL
+awk -F'\t' -v nom_table="$nom_table" '
+  NR == 1 {
+    formatted_line = "(";
+    for (i = 1; i <= NF; i++) {
+      formatted_line = formatted_line $i;
+      if (i < NF) {
+        formatted_line = formatted_line ", ";
+      }
+    }
+    formatted_line = formatted_line ")";
+  }
+
+  NR > 1 {
+    printf "INSERT INTO %s %s VALUES (", nom_table, formatted_line;
+
+    for (i = 1; i <= NF; i++) {
+      if ($i == "\\N") {
+        printf "NULL";
+      } else {
+        # Ajout de la condition pour détecter les tableaux
+        if (split($i, array_values, ",") > 1) {
+          printf "ARRAY[";
+          for (j = 1; j <= length(array_values); j++) {
+            printf "\047%s\047", array_values[j];
+            if (j < length(array_values)) {
+              printf ",";
+            }
+          }
+          printf "]";
+        } else {
+          printf "\047%s\047", $i;
+        }
+      }
+
+      if (i < NF) {
+        printf ",";
+      }
+    }
+    printf ");\n";
+  }
+' "$fichier_tsv" >> "$fichier_sql"
+
