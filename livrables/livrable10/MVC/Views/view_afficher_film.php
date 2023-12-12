00f2675777801e8019f970093127e07a8a9ffb95
Samy93000 <okisamycontactpro@gmail.com>
2023-12-12 22:37:36 +0100

    Update

--- livrables/livrable10/MVC/Views/view_afficher_film.php
+++ livrables/livrable10/MVC/Views/view_afficher_film.php
@@ -1,17 +1,14 @@
 <?php
-echo "<pre>";
-print_r($data);
-echo "</pre>";
-
 if (!empty($data)) {
     $film = $data[0];
     ?>
     <p>Title Type: <?php echo $film['titletype'] ?? 'non renseigné'; ?></p>
     <p>Primary Title: <?php echo $film['primarytitle'] ?? 'non renseigné'; ?></p>
     <p>Original Title: <?php echo $film['originaltitle'] ?? 'non renseigné'; ?></p>
-    <p>Is Adult: <?php echo $film['isadult'] ?? 'non renseigné'; ?></p>
+    <p>Is Adult: <?php echo $film['isadult'] !== null ? $film['isadult'] : 'non renseigné'; ?></p>
     <p>Start Year: <?php echo $film['startyear'] ?? 'non renseigné'; ?></p>
     <p>End Year: <?php echo $film['endyear'] ?? 'non renseigné'; ?></p>
+    <p>Runtime minutes: <?php echo $film['runtimeminutes'] ?? 'non renseigné'; ?></p>
     <?php 
       $genre_whithout_braces = str_replace(array('{', '}'), '', $film['genres']);
       $genre = explode(',', $genre_whithout_braces);
