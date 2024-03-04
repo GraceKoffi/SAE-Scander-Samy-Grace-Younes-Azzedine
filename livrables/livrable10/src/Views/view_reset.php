6d9505a468572e4139b1f9a0b7b9939c6744d533
Samy93000 <salu27oki@gmail.com>
2024-03-04 11:56:15 +0100

    Update

--- livrables/livrable10/src/Views/view_reset.php
+++ livrables/livrable10/src/Views/view_reset.php
@@ -1,9 +1,50 @@
-<form action='?controller=resetFinal&action=resetFinal&token=<?php echo $_GET['token']; ?>' method='post'>
-            <h2>Nouveau mot de passe</h2>    
-            <div class='form-group'>
-                    <label for='token'>Réinitialiser mot de passe</label>
-                        <input type='text' class='form-control' id='passWord' name='passWord'>
-                        <input type='submit' class='btn btn-primary submit-btn' value='Reinitialiser mot de passe'>
-                </div>
-            </form>
+<?php require "Views/view_navbar.php";?>
+
+<style>
+    .error{
+        margin-top: 100px;
+    }
+
+    .inputReset{
+        margin-top: 20px;
+    }
+
+    .labelReset{
+        margin-top:10px;
+    }
+
+    .resetForm{
+        margin-left: 10px;
+    }
+
+</style>
+
+<div class="container error">
+    <div class="row mx-auto align-items-center">
+        <div class="col">
+            <h1 class="titleError">Reinitilisation du mot de passe</h1>
+            <p class="errorParagraphe">Ici vous pouvez réinitiliser votre mot de passe</p>
+        </div>
+    </div>
+    <div class="container resetForm">
+        <div class="row align-items-center">
+            <div class="formulaire mx-auto col-md">
+                <form action='?controller=resetFinal&action=resetFinal&token=<?php echo $_GET['token']; ?>' method='post'> 
+                    <div class='form-group'>
+                            <label for='token' class="labelReset">Réinitialiser votre mot de passe</label>
+                                <div class="inputReset">
+                                    <input type='text' class='form-control' id='passWord' name='passWord'>
+                                </div>
+                                
+                                <div class="inputReset">
+                                    <input type='submit' class='btn btn-primary submit-btn' value='Reinitialiser mot de passe'>
+                                </div>
+                    </div>
+                
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
 
